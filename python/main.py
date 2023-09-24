1e5382d5c3f1c5a4d61213da8e64a83e5ffb4a06
Luis Guerreiro <luiscvlh11@gmail.com>
2023-09-24 00:04:31 -0300

    fix[python]: corrigido nomenclatura das funcoes

--- python/main.py
+++ python/main.py
@@ -19,23 +19,23 @@ class BinarySearchTree:
     def __init__(self, value: int):
         self.root = Node(value)
 
-    def add(self, value: int, node: Node | None = None):
+    def insert(self, value: int, node: Node | None = None):
         node = self.root if node is None else node
         if node.value > value:
             if node.left is None:
                 node.left = Node(value)
             else:
                 # go to the left
-                self.add(value, node.left)
+                self.insert(value, node.left)
         elif node.value < value:
             if node.right is None:
                 node.right = Node(value)
             else:
                 # go to the right
-                self.add(value, node.right)
+                self.insert(value, node.right)
         return
 
-    def search(self, value: int, node: Node | None = None):
+    def get_node_height(self, value: int, node: Node | None = None):
         node = self.root if node is None else node
         if node.value == value:
             return 0
@@ -43,24 +43,24 @@ class BinarySearchTree:
             if node.left is None:
                 raise ValueError()
             # go to the left
-            height = self.search(value, node.left) + 1
+            height = self.get_node_height(value, node.left) + 1
         elif node.value < value:
             if node.right is None:
                 raise ValueError()
             # go to the right
-            height = self.search(value, node.right) + 1
+            height = self.get_node_height(value, node.right) + 1
         return height
 
 
 if __name__ == "__main__":
     binary_search_tree = BinarySearchTree(50)
 
-    binary_search_tree.add(30)
-    binary_search_tree.add(70)
-    binary_search_tree.add(40)
-    binary_search_tree.add(60)
-    binary_search_tree.add(20)
-    binary_search_tree.add(80)
+    binary_search_tree.insert(30)
+    binary_search_tree.insert(70)
+    binary_search_tree.insert(40)
+    binary_search_tree.insert(60)
+    binary_search_tree.insert(20)
+    binary_search_tree.insert(80)
 
     # 0 degree
     assert binary_search_tree.root.value == 50
@@ -76,21 +76,21 @@ if __name__ == "__main__":
     assert binary_search_tree.root.right.right.value == 80
 
     # 0 degree
-    assert binary_search_tree.search(50) == 0
+    assert binary_search_tree.get_node_height(50) == 0
 
     # 1 degree
-    assert binary_search_tree.search(30) == 1
-    assert binary_search_tree.search(70) == 1
+    assert binary_search_tree.get_node_height(30) == 1
+    assert binary_search_tree.get_node_height(70) == 1
 
     # 2 degree
-    assert binary_search_tree.search(40) == 2
-    assert binary_search_tree.search(60) == 2
-    assert binary_search_tree.search(20) == 2
-    assert binary_search_tree.search(80) == 2
+    assert binary_search_tree.get_node_height(40) == 2
+    assert binary_search_tree.get_node_height(60) == 2
+    assert binary_search_tree.get_node_height(20) == 2
+    assert binary_search_tree.get_node_height(80) == 2
 
-    # inexistent value
+    # inexistent node
     try:
-        binary_search_tree.search(-100)
+        binary_search_tree.get_node_height(-100)
         raise AssertionError()
     except Exception as e:
         assert isinstance(e, ValueError)
