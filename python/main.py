1ac466374eeb32bbdfbc73558ed2a502ecd4c475
Luis Guerreiro <luiscvlh11@gmail.com>
2023-09-23 22:39:09 -0300

    feat[python]: adicionado metodo para buscar o grau de um valor na arvore binaria de busca

--- python/main.py
+++ python/main.py
@@ -35,6 +35,18 @@ class BinarySearchTree:
                 self.add(value, node.right)
         return
 
+    def search(self, value: int, node: Node | None = None):
+        node = self.root if node is None else node
+        if node.value == value:
+            return 0
+        elif node.value > value:
+            # go to the left
+            height = self.search(value, node.left) + 1
+        elif node.value < value:
+            # go to the right
+            height = self.search(value, node.right) + 1
+        return height
+
 
 if __name__ == "__main__":
     binary_search_tree = BinarySearchTree(50)
@@ -46,12 +58,28 @@ if __name__ == "__main__":
     binary_search_tree.add(20)
     binary_search_tree.add(80)
 
+    # 0 degree
     assert binary_search_tree.root.value == 50
 
+    # 1 degree
     assert binary_search_tree.root.left.value == 30
+    assert binary_search_tree.root.right.value == 70
+
+    # 2 degree
     assert binary_search_tree.root.left.right.value == 40
     assert binary_search_tree.root.left.left.value == 20
-
-    assert binary_search_tree.root.right.value == 70
     assert binary_search_tree.root.right.left.value == 60
     assert binary_search_tree.root.right.right.value == 80
+
+    # 0 degree
+    assert binary_search_tree.search(50) == 0
+
+    # 1 degree
+    assert binary_search_tree.search(30) == 1
+    assert binary_search_tree.search(70) == 1
+
+    # 2 degree
+    assert binary_search_tree.search(40) == 2
+    assert binary_search_tree.search(60) == 2
+    assert binary_search_tree.search(20) == 2
+    assert binary_search_tree.search(80) == 2
