b2280e7e24533407852fef0f5ca58aa31b9ab146
Luis Guerreiro <luiscvlh11@gmail.com>
2023-09-25 19:25:35 -0300

    feat[python]: adicionado remocao de um node

--- python/main.py
+++ python/main.py
@@ -92,6 +92,38 @@ class BinarySearchTree:
             _min = self.min(_node.left)
         return _min
 
+    def remove(
+        self,
+        value: int,
+        node: Node | None = None,
+    ):
+        _node = self.root if node is None else node
+        # go to the left
+        if _node.value > value:
+            if _node.left is None:
+                raise ValueError()
+            _node.left = self.remove(value, _node.left)
+        # go to the right
+        elif _node.value < value:
+            if _node.right is None:
+                raise ValueError()
+            _node.right = self.remove(value, _node.right)
+        # first case, where node is a leaf
+        elif _node.left is None and _node.right is None:
+            return None
+        # second case, where node hasn't left
+        elif _node.left is None:
+            return _node.right
+        # second case, where node hasn't right
+        elif _node.right is None:
+            return _node.left
+        #  third case, where node has both left and right
+        else:
+            successor = self.min(_node.right)
+            _node.value = successor
+            _node.right = self.remove(successor, _node.right)
+        return _node
+
 
 if __name__ == "__main__":
     binary_search_tree = BinarySearchTree(50)
@@ -163,3 +195,34 @@ if __name__ == "__main__":
 
     # min value in the three
     assert binary_search_tree.min() == 20
+
+    def make_bst():
+        bst = BinarySearchTree(8)
+
+        bst.insert(3)
+        bst.insert(10)
+        bst.insert(1)
+        bst.insert(6)
+        bst.insert(14)
+        bst.insert(4)
+        bst.insert(7)
+        bst.insert(13)
+
+        return bst
+
+    # remove leaf
+    bst = make_bst()
+    bst.remove(1)
+    assert bst.root.left.left is None
+
+    # remove node with one child
+    bst = make_bst()
+    bst.remove(10)
+    assert bst.root.right.value == 14
+
+    # remove root node
+    bst = make_bst()
+    bst.remove(8)
+    assert bst.root.value == 10
+    assert bst.root.left.value == 3
+    assert bst.root.right.value == 14
